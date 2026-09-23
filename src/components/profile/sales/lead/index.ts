996a632920c45057132f33d0e2d4ddbac55fad10
Marco Castañeda <marco_pascual410@hotmail.com>
2026-09-23 11:40:39 -0600

    fix: whatsapp panel

--- src/components/profile/sales/lead/index.ts
+++ src/components/profile/sales/lead/index.ts
@@ -1,1 +1,7 @@
 export { default as AddLeadSection } from "./AddLeadSection";
+export {
+  ADD_OWN_LEAD_MUTATION,
+  type AddOwnLeadInput,
+  type AddOwnLeadResult,
+  type AddOwnLeadVariables,
+} from "./queries";
